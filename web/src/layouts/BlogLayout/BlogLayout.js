36228e9ab069e565cc9e3afcba06fb15afdfa17d
Nathan <nathanflores887@gmail.com>
2023-04-23 19:27:08 +0800

    feat(forms): Add contact page with forms

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,8 +1,11 @@
+import { routes, Link } from '@redwoodjs/router'
+
 const BlogLayout = ({ children }) => {
   return (
     <>
       <h1>THIS IS A LAYOUT :D</h1>
       {children}
+      <Link to={routes.contact()}>Contact page</Link>
     </>
   )
 }
---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,32 @@
+import {
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+} from '@redwoodjs/forms'
+import { MetaTags } from '@redwoodjs/web'
+
+const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <>
+      <MetaTags title="Contact" description="Contact page" />
+
+      <Form onSubmit={onSubmit} className="form">
+        <TextField name="name" validation={{ required: true }} />
+        <FieldError name="name" className="error" />
+        <TextField name="email" validation={{ required: true }} />
+        <FieldError name="email" className="error" />
+        <TextAreaField name="message" validation={{ required: true }} />
+        <FieldError name="message" className="error" />
+        <Submit>Send Message</Submit>
+      </Form>
+    </>
+  )
+}
+
+export default ContactPage
